7f1061cea462da7ecda197c261464fee8df0c303
gere149 <gere.delatorre@gmail.com>
2025-02-21 16:56:36 +0100

    Localization Implemented

---
+++ Assets/Scripts/Localization/LanguageDropdown.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using TMPro;
+
+public class LanguageDropdown : MonoBehaviour
+{
+    private TMP_Dropdown languageDropdown;
+
+    private void Start()
+    {
+        languageDropdown = GetComponent<TMP_Dropdown>(); 
+        languageDropdown.value = (int)Localizer.GetCurrentLanguage() - 1;
+        languageDropdown.onValueChanged.AddListener(OnDropdownValueChanged);
+    }
+
+    private void OnDropdownValueChanged(int selectedIndex)
+    {
+        Language selectedLanguage = (Language)selectedIndex + 1;
+        Localizer.SetLanguage(selectedLanguage);
+        Debug.Log("Idioma cambiado a: " + selectedLanguage);
+    }
+}
---
+++ Assets/Scripts/Localization/LocalizeText.cs
@@ -0,0 +1,29 @@
+﻿using TMPro; 
+using UnityEngine;
+
+public class LocalizeText : MonoBehaviour
+{
+    public string TextKey;
+    private TMP_Text _textValue;  
+
+    void Start()
+    {
+        _textValue = GetComponent<TMP_Text>(); 
+        _textValue.text = Localizer.GetText(TextKey);
+    }
+
+    private void OnEnable()
+    {
+        Localizer.OnLanguageChange += ChangeLanguage;
+    }
+
+    private void OnDisable()
+    {
+        Localizer.OnLanguageChange -= ChangeLanguage;
+    }
+
+    private void ChangeLanguage()
+    {
+        _textValue.text = Localizer.GetText(TextKey);  
+    }
+}
