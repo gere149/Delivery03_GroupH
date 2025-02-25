6137a209164011c0fb03e681ba45d68eb9167d00
gere149 <gere.delatorre@gmail.com>
2025-02-25 09:30:29 +0100

    Scene Manage and background
    Title and Ending.

--- Assets/Scripts/Managers/InitialCanvasManager.cs
+++ Assets/Scripts/Managers/InitialCanvasManager.cs
@@ -2,7 +2,6 @@ using UnityEngine;
 
 public class InitialCanvasManager : MonoBehaviour
 {
-    [SerializeField] private GameObject infoPanel;
     [SerializeField] private GameObject startPanel;
 
     private void Start()
@@ -13,12 +12,10 @@ public class InitialCanvasManager : MonoBehaviour
     public void EnablePanelInfo()
     {
         startPanel.SetActive(false);
-        infoPanel.SetActive(true);
     }
 
     public void DisablePanelInfo()
     {
         startPanel.SetActive(true);
-        infoPanel.SetActive(false);
     }
 }
